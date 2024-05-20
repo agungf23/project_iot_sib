029230edb6e24d4880c30aab1827433f214bcb51
King Agung <kingagungks23@gmail.com>
2024-05-20 19:39:29 +0700

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import Alpine from 'alpinejs';
+
+// const vite=require("boostrap");
+// const laravel=require ("alpinejs");
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+// const vite=require("vite");
+// const laravel=require ("laravel-vite-plugin");
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    css: {
+        postcss: './postcss.config.js',
+      },
+});
