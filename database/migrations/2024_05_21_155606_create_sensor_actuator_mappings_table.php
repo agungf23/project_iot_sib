b070bada5ce70160168b4ff5484ba30fe55ba4c2
King Agung <kingagungks23@gmail.com>
2024-05-21 23:00:29 +0700

    fiks migrate tabel.php

---
+++ database/migrations/2024_05_21_155606_create_sensor_actuator_mappings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_actuator_mappings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sensor_id')->constrained('sensors')->cascadeOnDelete();
+            $table->foreignId('actuator_id')->constrained('actuators')->cascadeOnDelete();
+            $table->int('trigger_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_actuator_mappings');
+    }
+};
