08cbcc7548e72a0ba3068a95ac648924d2b538bd
King Agung <kingagungks23@gmail.com>
2024-05-21 15:37:50 +0700

    delete all migrations

--- database/migrations/2024_05_21_083633_create_sensors_table.php
+++ database/migrations/2024_05_21_083633_create_sensors_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('moistures', function (Blueprint $table) {
+        Schema::create('sensors', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['temperature', 'humidity', 'moisture', 'intensity']);
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('moistures');
+        Schema::dropIfExists('sensors');
     }
 };
